fbf036193b7f9212521cb55bd7ba092240fa1626
ishudahiya2001 <78587766+ishudahiya2001@users.noreply.github.com>
2022-03-08 22:38:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "Prod-server" {
-   ami = "ami-076754bea03bde973" 
+   ami = "ami-0b0af3577fe5e3532" 
    instance_type = "t2.micro" 
    
        tags = {
