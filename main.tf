38046e9941d74f07c98c272977e8bd4b77f90fc9
ishudahiya2001 <78587766+ishudahiya2001@users.noreply.github.com>
2022-03-08 21:57:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "wt7L5CZtHkjFv1wKqdwk74+/k/4CGTTVrmCzd0LU"
 }
 
-resource "aws_instancce" "Prod-server" {
+resource "aws_instance" "Prod-server" {
    ami = "ami-0c293f3f676ec4f90"
    instance_type = "t2.micro" 
    
